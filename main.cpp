631e71e5a9a789bbc1e29d9df09d1f009f13d11d
Sharon Rocha <srochav@frba.utn.edu.ar>
2026-02-09 23:01:50 -0300

    Add main.cpp to print greeting message

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Esta línea imprime el saludo y deja el cursor abajo como pide el TP
+    cout << "Hola Sharon Rocha" << endl; 
+    return 0;
+}
